07b736166c10c44087facfeddd1983e6932ace31
amey <mahajaniameyvishnu@gmail.com>
2026-04-01 20:40:17 +0000

    folder structure created

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/__init__.py
+touch research/trials.ipynb
+touch .env
+touch app.py
+touch requirements.txt
+touch README.md
+touch setup.py
+
+echo "Project structure created successfully!"
